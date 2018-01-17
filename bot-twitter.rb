bbffc611d7d6730dadf7f95434da6ec8dce14c54
bseb <bailly93@gmail.com>
2018-01-17 16:56:07 +0100

    commit avec recup tweet coffe et tea

--- bot-twitter.rb
+++ bot-twitter.rb
@@ -10,5 +10,17 @@ client = Twitter::REST::Client.new do |config|
   config.access_token        = ENV['my_access_token']
   config.access_token_secret = ENV['my_access_token_secret']
 end
-p client
-client.update('Mon 2eme twitte bot, merci TheHackingProject!!!!!!!! ')
+
+client_stream = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = ENV['my_consumer_key']
+  config.consumer_secret     = ENV['my_consumer_secret']
+  config.access_token        = ENV['my_access_token']
+  config.access_token_secret = ENV['my_access_token_secret']
+end
+
+topics = ["coffee", "tea"]
+client_stream.filter(track: topics.join(",")) do |object|
+  puts object.text if object.is_a?(Twitter::Tweet)
+end
+
+#client.update('Mon 3eme twitte bot, merci TheHackingProject!!!!!!!!')
